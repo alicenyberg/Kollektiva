50b933d5a7adcd2cf50e1860a08f9857b550542d
alicenyberg <alice.nyberg@hotmail.com>
2022-04-14 14:53:26 +0200

    footer styling

--- Kollektiva/resources/views/boilerplate/footer.blade.php
+++ Kollektiva/resources/views/boilerplate/footer.blade.php
@@ -1,2 +1,19 @@
+<footer>
+    <div class="wrapper">
+        <div class="box-1">
+            <p>Kundtjänst</p>
+            <p>071 -234 56 78</p>
+            <p>info@kollektiva.se</p>
+        </div>
+        <div class="box-2">
+            <a href="#">MENY 1</a>
+            <a href="#">MENY 2</a>
+            <a href="#">MENY 3</a>
+        </div>
+        <div class="box-3">
+            <button type="button">Logga in</button>
+            <button type="button">Registrera dig</button>
+        </div>
+    </div>
+</footer>
 </body>
-<footer></footer>
