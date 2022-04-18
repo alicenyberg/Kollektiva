23429ef18fb147d379771aa6535521f1dc67b5d7
alicenyberg <alice.nyberg@hotmail.com>
2022-04-18 17:39:34 +0200

    how to-section almost done

--- Kollektiva/resources/views/start.blade.php
+++ Kollektiva/resources/views/start.blade.php
@@ -8,19 +8,34 @@
         <button>Jag söker bostad</button>
         <img src="{{url('/images/start.jpeg')}}" alt="Image" />
     </div>
-    <div>
+    <section class="how-to-section">
         <h2>Hur gör man?</h2>
-        <div class="guide-container">
-            <div class="circles">1</div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            <div class="circles">2</div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            <div class="circles">3</div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <div class="how-to">
+            <div class="how-to-wrapper">
+                <div class="circle">
+                    <h3>1</h3>
+                </div>
+                <div class="vertical-line"></div>
+                <div class="circle">
+                    <h3>2</h3>
+                </div>
+                <div class="vertical-line"></div>
+                <div class="circle">
+                    <h3>3</h3>
+                </div>
+            </div>
+            <div class="how-to-steps">
+                <p>Lorem ipsum dolor sit amet, consectetur elit.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur elit.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur elit.</p>
+            </div>
+
         </div>
-        <!-- <img src="{{url('/images/start-two.png')}}" alt="Image" />
-        <button>Registrera dig nu</button> -->
-    </div>
+        <div class="register-now">
+            <img src="{{url('/images/start-two.png')}}" alt="Image" />
+            <button type="submit">Registrera dig nu</button>
+        </div>
+    </section>
 </div>
 
 
