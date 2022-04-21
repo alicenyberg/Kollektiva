4da9ce9fb57c52beb554157454fdbc8ae97fd757
DavisDavisDavis <olizoooom@gmail.com>
2022-04-21 21:38:06 +0200

    Information needed for matchmaking is set ✅

--- Kollektiva/resources/views/matchmaking.blade.php
+++ Kollektiva/resources/views/matchmaking.blade.php
@@ -2,11 +2,19 @@
 
 use App\Models\Residence;
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
+
+/*
+Yes I know this is spaghetti AND carbonara
+but it works for the demo
+and we want to avoid people having to create an account to test out the product
+*/
 
 $users = User::all();
-// die(var_dump($users));
-// die(var_dump($users->getOriginal()));
+$residence = DB::table('residences')->latest('id')->first();
 
+
+die(var_dump($residence));
 ?>
 
 @include('/boilerplate/header')@include('/boilerplate/header')
@@ -14,6 +22,37 @@ $users = User::all();
 <section class="matchmakingSection">
     <h2>Dessa är de ansökande</h2>
 
+    <h2>Dessa har matchat dig bäst!</h2>
+    <section class="find-residence-section">
+            <?php foreach ($users as $user) : ?>
+                <?php if (condition): ?>
+                    <div class="residence-container">
+                    <img src="{{$user->image}}" alt="">
+                    <h4><?= $user->name ?></h4>
+                    <ul>
+                        <?php if ($user->partying  == "true"): ?>
+                                <li>Party - ✅</li>
+                            <?php else: ?>
+                                <li>Party - ❌</li>
+                        <?php endif; ?>
+                        <?php if ($user->smoking  == "true"): ?>
+                                <li>Rökning - ✅</li>
+                            <?php else: ?>
+                                <li>Rökning - ❌</li>
+                        <?php endif; ?>
+                        <?php if ($user->animals  == "true"): ?>
+                                <li>Husdjur - ✅</li>
+                            <?php else: ?>
+                                <li>Husdjur - ❌</li>
+                        <?php endif; ?>
+                    </ul>
+                    <button>Skapa kontakt</button>
+                <?php endif; ?>
+            </div>
+        <?php endforeach; ?>
+    </section>
+
+    <h2>uwuwuwu</h2>
         <section class="find-residence-section">
             <?php foreach ($users as $user) : ?>
                 <div class="residence-container">
@@ -36,7 +75,6 @@ $users = User::all();
                             <li>Husdjur - ❌</li>
                     <?php endif; ?>
                 </ul>
-
                 <button>Skapa kontakt</button>
             </div>
         <?php endforeach; ?>
